e035ef7328cd98064752322a9e68445cdef302fa
estebanrfp <estebanrfp@gmail.com>
2017-04-26 09:01:42 +0200

    update project

--- app/auto.js
+++ app/auto.js
@@ -1,12 +1,13 @@
 const git = require('simple-git')
 const firebase = require('firebase')
 const config = require('./config')
+const childProcess = require('child_process')
 
 firebase.initializeApp(config)
 
 var ref = firebase.database().ref().child("servers")
 
-ref.child(config.device).on('value', autoPull)
+// ref.child(config.device).on('value', autoPull)
 
 // process.on('SIGINT', function() {
 //   process.exit()
@@ -14,11 +15,11 @@ ref.child(config.device).on('value', autoPull)
 
 var running = false
 
-// setInterval(function() {
-//   if (running == true) return false;
-//   running = true
-//   autoPull()
-// }, config.interval || 30000) // 30000
+setInterval(function() {
+  if (running == true) return false;
+  running = true
+  autoPull()
+}, config.interval || 30000) // 30000
 
 function autoPull (data) {
   // console.log(data.val())
@@ -29,6 +30,7 @@ function autoPull (data) {
     .pull(function(err, update) {
       if(update && update.summary.changes) {
         console.log('processing and restarting app ...')
+        require('child_process').exec('npm rebuild')
       }
     })
     .then(function() {
