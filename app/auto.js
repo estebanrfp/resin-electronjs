c5eb0e99df2236e2f82128398a56b150db718738
estebanrfp <estebanrfp@gmail.com>
2017-04-26 18:12:04 +0200

    update auto.js

--- app/auto.js
+++ app/auto.js
@@ -29,8 +29,8 @@ function autoPull (data) {
     })
     .pull(function(err, update) {
       if(update && update.summary.changes) {
-        console.log('processing and restarting app ...')
-        require('child_process').exec('node_modules/.bin/electron-rebuild')
+        console.log('processing and restarting app ... :-)')
+        // require('child_process').exec('node_modules/.bin/electron-rebuild')
       }
     })
     .then(function() {
